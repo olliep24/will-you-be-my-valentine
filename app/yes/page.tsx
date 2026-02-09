853fd8e134f42cc254062feac1b55f487d507d0e
Oliver Petrick <odpetrick@berkeley.edu>
2026-02-08 18:48:35 -0800

    Created website

---
+++ app/yes/page.tsx
@@ -0,0 +1,41 @@
+export default function YesPage() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4 bg-white font-sans">
+      <h1 className="text-4xl font-bold text-black">yayyyy</h1>
+      <div className="grid grid-cols-4 gap-4 mt-4">
+        <img
+          src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExdHlqeDJrb3J6azAzcnRpd2l3MnIxc3JnbmZxNWI2Zjhqdjk3NzM0dyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/uv0VSp6Eqlu1nGvkZ0/giphy.gif"
+          alt="Valentine gif"
+          className="w-48 h-48 rounded-lg object-cover"
+        />
+        <img
+          src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMnEwbTM3bTVqMDdrNDlweWxqdzc2eHpkcGxwMzRnMTZ2aWR4YjhkeCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/Z5SMZDDtlPgL6/giphy.gif"
+          alt="Valentine gif"
+          className="w-48 h-48 rounded-lg object-cover"
+        />
+        <img
+          src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZXNqcWlqdDJuamR3ZzA4bGZkcXU5dDI5cnJlcnQxMW1uNHExNjJrbSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/HdDIQPqGyFvsA/giphy.gif"
+          alt="Valentine gif"
+          className="w-48 h-48 rounded-lg object-cover"
+        />
+        <img
+          src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMnEwbTM3bTVqMDdrNDlweWxqdzc2eHpkcGxwMzRnMTZ2aWR4YjhkeCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/8FGvwxpd9rrvW/giphy.gif"
+          alt="Valentine gif"
+          className="w-48 h-48 rounded-lg object-cover"
+        />
+        <img src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOGdkbnI4ZDBxaWZycG9oeDI2MWRwcWplNXQ2YmN6MWl6NjV5dHE4dyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/ytu2GUYbvhz7zShGwS/giphy.gif" alt="Valentine gif" className="w-48 h-48 rounded-lg object-cover" />
+        <img src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOHdxMmJuZnJkdnN5bDc2dTZyYTljNDEyc3FsMXdncDU1bGpkYzIwaCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/LBMiDWkCw0e53UOcm0/giphy.gif" alt="Valentine gif" className="w-48 h-48 rounded-lg object-cover" />
+        <img src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOGdmYWgyamhrNDBrM2I0cmhxeG9xenZtZWUwZHF0cGF5azU2YXM2byZlcD12MV9naWZzX3NlYXJjaCZjdD1n/P7jtQ5KzfTyl6u3S7I/giphy.gif" alt="Valentine gif" className="w-48 h-48 rounded-lg object-cover" />
+        <img src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZ2ZvYnl3Zzhkdm84NmtwcnFyZmtpa20wcjFzODRkOHV6ZXBpYzh4YiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/v6aOjy0Qo1fIA/giphy.gif" alt="Valentine gif" className="w-48 h-48 rounded-lg object-cover" />
+        <img src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMnEwbTM3bTVqMDdrNDlweWxqdzc2eHpkcGxwMzRnMTZ2aWR4YjhkeCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/10fUNsbuaYB8eA/giphy.gif" alt="Valentine gif" className="w-48 h-48 rounded-lg object-cover" />
+        <img src="https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3cnMwdzdjajkyeDVjamM1YnVrbWpudTJtYWxkaHQyazhtOW53dXluMSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/Yle9Yz9izeVRyiwavn/giphy.gif" alt="Valentine gif" className="w-48 h-48 rounded-lg object-cover" />
+        <img src="https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3NXAybzY2bTlrOWZldXdjdjBocnY5MHN4cGQ4djJzZHlrdHlmNmx6MSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/6hJgrRvYS1LgS3aY0U/giphy.gif" alt="Valentine gif" className="w-48 h-48 rounded-lg object-cover" />
+        <img src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMmI0YzJjOWRrNWI3YmswMWF5ejFpOGZlYzJtcTI5MmI0a285bHV5NCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/1JmGiBtqTuehfYxuy9/giphy.gif" alt="Valentine gif" className="w-48 h-48 rounded-lg object-cover" />
+        <img src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMGZrcmFxdWtkN2E2NnlzYnA4ZzBuaGZudmF3N3d2YmRvZW4zN2VpayZlcD12MV9naWZzX3NlYXJjaCZjdD1n/PzS6gzZ14nACk/giphy.gif" alt="Valentine gif" className="w-48 h-48 rounded-lg object-cover" />
+        <img src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMmI0YzJjOWRrNWI3YmswMWF5ejFpOGZlYzJtcTI5MmI0a285bHV5NCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/chpVje4XVoDLnDQ7XJ/giphy.gif" alt="Valentine gif" className="w-48 h-48 rounded-lg object-cover" />
+        <img src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOGdmYWgyamhrNDBrM2I0cmhxeG9xenZtZWUwZHF0cGF5azU2YXM2byZlcD12MV9naWZzX3NlYXJjaCZjdD1n/zZdNDZbbbgJAaGu1lZ/giphy.gif" alt="Valentine gif" className="w-48 h-48 rounded-lg object-cover" />
+        <img src="https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3OGRzNzZmeTAxdWlrcmtteDVnM2pod2Z1OTFybDZwdnAyaGthajVxaSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/vDhDcIEmShbUI/giphy.gif" alt="Valentine gif" className="w-48 h-48 rounded-lg object-cover" />
+      </div>
+    </div>
+  );
+}
